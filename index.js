ffecaa1df07ba89e2948002bd44948d3c53d16db
Your Name <kkamadi567@gmail.com>
2023-10-05 08:44:25 +0300

    changes

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-function addingEventListener() {
+// function clickAlert(){
+//   alert("I was clicked!");
+// }
+
+function addingEventListener(butt){
+  const element =document.getElementById("input");
+  if ("button"){
+    element.addEventListener("click", function(){
+      alert("Button clicked!");
+    })
+  }
 }
+  // document.addEventListener("DOMContentLoaded", function(){  
+  // const input = document.getElementById("input");
+
+//   input.addEventListener("click", function() {
+//    alert("I was clicked!");
+// });
+// return true;
+// })
+// addingEventListener();
+
+// const button = document.getElementById("button");
+// button.addEventListener("click", clickAlert);
+// }
+
+// addEventListener()
